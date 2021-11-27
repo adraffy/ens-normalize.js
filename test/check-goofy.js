6eba386c2ff5d993b538084a3483da8858de046a
Andrew Raffensperger <raffy@me.com>
2021-11-26 22:49:13 -0800

    changed from json to html reports

--- test/check-goofy.js
+++ test/check-goofy.js
@@ -8,28 +8,22 @@ function local_file(name) {
 	return new URL(name, import.meta.url).pathname;
 }
 
-function code_str(v) {
-	return [...v].map(x => x.codePointAt(0).toString(16).padStart(2, '0')).join(' ');
-}
-
-let labels = readFileSync(local_file('goofy-labels.txt'), {encoding: 'utf8'}).split('\n');
 let errors = [];
 let diffs = [];
 
+let labels = readFileSync(local_file('goofy-labels.txt'), {encoding: 'utf8'}).split('\n');
 for (let label of labels) {
 	try {
-		let norm = ens_normalize(label);
+		let norm = ens_normalize(label, true);
 		if (norm != label) {
-			diffs.push({
-				label: [label, code_str(label)], 
-				norm: [norm, code_str(norm)]
-			});
+			diffs.push({label, norm});
 		}
 	} catch (err) {
 		errors.push({label, error: err.message});
 	}
 }
 
+/*
 console.log({
 	errors: errors.length,
 	diffs: diffs.length
@@ -37,6 +31,39 @@ console.log({
 
 writeFileSync(local_file('output/goofy-errors.json'), JSON.stringify(errors));
 writeFileSync(local_file('output/goofy-diffs.json'), JSON.stringify(diffs));
+*/
 
+function code_str(v) {
+	return '<tt>' + (v.length == 0 ? '<i>empty</i>' : [...v].map(x => x.codePointAt(0).toString(16).padStart(2, '0')).join(' ')) + '</tt>';
+}
 
+function html_escape(s) {
+	return s.replace( /[^0-9a-z ]/iug, x => `&#${x.codePointAt(0)}`);
+}
 
+let html = `<!doctype html><html><head><meta charset="utf-8"><style>
+table { border-collapse: collapse; }
+td { border: 1px solid #ccc; }
+tbody tr:nth-child(4n+1), tbody tr:nth-child(4n+2) { background: #eee; }
+.name { line-break: anywhere; }
+</style></head><body>`;
+html += `<h2>Different (${diffs.length} of ${labels.length})</h2>`;
+if (diffs.length > 0) {
+	html += `<table><thead><th>Name</th><th>Codepoints</th><tbody>`;
+	for (let {label, norm} of diffs) {
+		html += `<tr><td class="name">${html_escape(label)}</td><td>${code_str(label)}</td></tr>`;
+		html += `<tr><td class="name">${html_escape(norm)}</td><td>${code_str(norm)}</td>`;
+	}
+	html += '</tbody></table>';
+}
+html += `<h2>Error (${errors.length} of ${labels.length})</h2>`;
+if (errors.length > 0) {
+	html += `<table><thead><th>Name</th><th>Codepoints</th><th>Error</th></thead><tbody>`;
+	for (let {label, error} of errors) {
+		html += `<tr><td class="name">${html_escape(label)}</td><td>${code_str(label)}</td><td>${error}</td></td>`;
+	}
+	html += '</tbody></table>';
+}
+html += `</body></html>`
+
+writeFileSync(local_file('output/goofy.html'), html);
--- test/compare-ethers.js
+++ test/compare-ethers.js
@@ -1,21 +1,86 @@
 import {nameprep} from '@ethersproject/strings';
 import {ens_normalize} from '../ens-normalize.js';
+
 import {KNOWN} from './known.js';
+import {readFileSync, writeFileSync} from 'fs';
 
-for (let {name, norm, error} of KNOWN) {
-	
-	if (!norm) norm = name;
+function local_file(name) {
+	return new URL(name, import.meta.url).pathname;
+}
+
+function safe_adraffy(name) {
 	try {
-		let norm1 = ens_normalize(name);
-		let norm2 = nameprep(name);
-		if (norm1 != norm2) {
-
-		}
-		
+		return {norm: ens_normalize(name, true)};
 	} catch (err) {
-		if (!error) {
-			console.error(err);
-			process.exit(1);
-		}
+		return {error: err.message};
 	}
 }
+
+function safe_ethers(name) {
+	try {
+		return {norm: nameprep(name)};
+	} catch (err) {
+		return {error: err.message};
+	}
+}
+
+let results = [];
+
+for (let {name} of KNOWN) {	
+	let adraffy = safe_adraffy(name);
+	let ethers = safe_ethers(name);
+	let type;
+	if (!adraffy.error != !ethers.error) {
+		type = 'disagree';
+	} else if (adraffy.error) {
+		type = 'both-error';
+	} else if (adraffy.norm != ethers.norm) {
+		type = 'diff-norm';
+	} else {
+		type = 'same-norm';
+	}
+	results.push({type, name, adraffy, ethers});
+}
+
+function code_str(v) {
+	return '<tt>' + [...v].map(x => x.codePointAt(0).toString(16).padStart(2, '0')).join(' ') + '</tt>';
+}
+
+function html_escape(s) {
+	return s.replace( /[^0-9a-z ]/iug, x => `&#${x.codePointAt(0)}`);
+}
+
+let html = `<!doctype html><html><head><meta charset="utf-8"><style>
+table { border-collapse: collapse; }
+td { border: 1px solid #ccc; }
+.same { background-color: #eee; }
+.diff { background-color: #ffc; }
+.fail { background-color: #fcc; }
+</style></head><body>`;
+
+function html_for_cell(name, info) {
+	let {norm, error} = info;
+	if (error) {
+		return `<td class="fail" colspan="2">${error}</td>`;
+	} else if (norm === name) {
+		return `<td class="same" colspan="2">${html_escape(norm)}</td>`;
+	} else {
+		return `<td class="diff">${html_escape(norm)}</td><td class="diff">${code_str(norm)}</tt></td>`;
+	}
+}
+
+for (let type of [...new Set(results.map(x => x.type))].sort((a, b) => a.localeCompare(b))) {
+	let list = results.filter(x => x.type == type);
+	html += `<h2>${type} (${list.length})</h2>`;
+	html += `<table><thead><th colspan="2">Name</th><th colspan="2">adraffy</th><th colspan="2">ethers</th></thead><tbody>`;
+	for (let {name, adraffy, ethers} of list) {
+		html += `<tr><td>${html_escape(name)}</td><td>${code_str(name)}</td>`;
+		html += html_for_cell(name, adraffy);
+		html += html_for_cell(name, ethers);
+		html += `</tr>`;
+	}
+	html += '</tbody></table>';	
+}
+html += `</body></html>`
+
+writeFileSync(local_file('output/compare-ethers.html'), html);
