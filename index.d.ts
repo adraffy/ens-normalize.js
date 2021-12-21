f7279823bafe60d2125c33417ebb8cdd08f9ae9b
Andrew Raffensperger <raffy@me.com>
2021-12-21 04:59:46 -0800

    fixed ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export function ens_normalize(name: string, check_bidi?: boolean): string;
+export function ens_normalize(name: string): string;
 
 export const VERSION: string;
 export const UNICODE: string;
