1f5e50d83938bcf96c22ce96f0d7e0059ff2a8a3
Andrew Raffensperger <raffy@me.com>
2021-12-21 04:55:04 -0800

    updated readme

--- build/lib-bidi.js
+++ build/lib-bidi.js
@@ -2,6 +2,8 @@ import {is_bidi_label, validate_bidi} from './bidi.js';
 import {label_error} from './utils.js';
 
 export function check_bidi(name) {
+	// TODO: fix this
+	// determine if emoji interact with bidi
 	let labels = name.split('.').map(s => [...s].map(x => x.codePointAt(0)));
 	if (labels.some(is_bidi_label)) {
 		for (let cps of labels) {
