9a153bc67fb7793dde418838cdb0aa68074dc666
Andrew Raffensperger <raffy@me.com>
2022-09-25 01:57:29 -0700

    minor fixes

--- src/lib.js
+++ src/lib.js
@@ -18,7 +18,7 @@ const ISOLATED = read_valid_subset();
 const SCRIPTS = ['Latin', 'Greek', 'Cyrillic'].map((k, i) => {
 	// this defines the priority
 	// order must match make.js
-	// (script name, script-set, whole-set)
+	// (script name, script-set, whole-set?)
 	return [k, read_valid_subset(), i ? read_valid_subset() : 0];
 });
 const EMOJI_SOLO = new Set(read_member_array(r));
