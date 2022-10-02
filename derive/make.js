0eb66c92e7ac3ead95b6914b5c3151501b5bc2ec
Andrew Raffensperger <raffy@me.com>
2022-10-02 00:02:33 -0700

    change flags to RGI instead of spec-derived RGI

--- derive/make.js
+++ derive/make.js
@@ -99,10 +99,6 @@ function register_emoji(info) {
 }
 
 function set_isolated(cp) {
-	if (!valid.has(cp)) {
-		// this shouldn't be fatal
-		console.log(`Warning: Isolated not Valid: ${UNICODE.format(cp)}`);
-	}
 	if (isolated.has(cp)) {
 		throw new Error(`Already Isolated: ${UNICODE.format(cp)}`);
 	}
@@ -118,6 +114,11 @@ emoji_seqs.Emoji_Keycap_Sequence.forEach(register_emoji);
 emoji_seqs.RGI_Emoji_Tag_Sequence.forEach(register_emoji);
 emoji_seqs.RGI_Emoji_Modifier_Sequence.forEach(register_emoji);
 
+// derive flag sequences with valid regions
+// warning: this contains EZ and QO
+// UNICODE.valid_emoji_flag_sequences().forEach(register_emoji);
+emoji_seqs.RGI_Emoji_Flag_Sequence.forEach(register_emoji); // use this instead
+
 let emoji_chrs = UNICODE.emoji_data();
 let emoji_map = new Map(emoji_chrs.Emoji.map(x => [x.cp, x]));
 let emoji_demoted = new Set((await import('./rules/emoji-demoted.js')).default);
@@ -130,12 +131,11 @@ for (let rec of emoji_map.values()) {
 	}
 }
 
-// flag sequences with valid regions
-UNICODE.valid_emoji_flag_sequences().forEach(register_emoji);
 // disable single regionals
 for (let cp of Regional_Indicator) {
 	emoji_map.get(cp).used = true;
 }
+
 // register forced FE0F
 for (let info of emoji_seqs.Basic_Emoji) {
 	if (info.cps.length == 2 && info.cps[1] == 0xFE0F) { // X + FE0F
@@ -146,15 +146,16 @@ for (let info of emoji_seqs.Basic_Emoji) {
 		register_emoji(info);
 	}
 }
+
 // register default emoji-presentation
 for (let info of emoji_chrs.Emoji_Presentation) {
-	//if (Regional_Indicator.has(info.cp)) continue; // skipped (not necessary with above flag logic)
 	let rec = emoji_map.get(info.cp);
 	if (!rec) throw new Error(`Expected emoji: ${UNICODE.format(info)}`);	
 	if (rec.used) continue;
 	rec.used = true;
 	register_emoji({cps: [info.cp, 0xFE0F], ...info});
 }
+
 // register default text-presentation (leftovers)
 for (let info of emoji_map.values()) {
 	if (!info.used) {	
@@ -274,9 +275,8 @@ for (let info of emoji.values()) {
 }
 for (let cp of isolated) {	
 	if (!valid.has(cp)) {
-		// shouldn't be fatal, see: set_isolated()
-		//throw new Error(`Isolated not Valid: ${UNICODE.format(cp)}`);
 		isolated.delete(cp);
+		console.log(`*** Isolated not Valid: ${UNICODE.format(cp)}`); // non fatal
 	}
 	if (cc.has(cp)) {
 		throw new Error(`Isolated with non-zero combining class: ${UNICODE.format(cp)}`);
@@ -303,6 +303,7 @@ for (let abbr of SCRIPTS.excluded()) {
 	console.log(`Excluded Script: ${abbr} (${decomposed.size})`);
 }
 
+
 // wholes
 let wholes_Grek = (await import('./rules/confusables-Grek.js')).default.filter(cp => valid.has(cp));
 let wholes_Cyrl = (await import('./rules/confusables-Cyrl.js')).default.filter(cp => valid.has(cp));
@@ -319,18 +320,20 @@ writeFileSync(new URL('./spec.json', out_dir), JSON.stringify({
 	cm: sorted(cm),
 	emoji: [...emoji.values()].map(x => x.cps).sort(compare_arrays),
 	isolated: sorted(isolated),
+	excluded,	
+	// TODO: generalize wholes+scripts to [Latn, Grek, Cryl]
 	scripts: {
 		Latn: sorted(scripts.Latn),
 		Grek: sorted(scripts.Grek),
 		Cyrl: sorted(scripts.Cyrl),
 	}, 
-	excluded,
 	wholes: {
 		Grek: sorted(wholes_Grek),
 		Cyrl: sorted(wholes_Cyrl),
 	}
 }));
 
+// this file should be independent so we can create a standalone nf implementation
 writeFileSync(new URL('./nf.json', out_dir), JSON.stringify({
 	created,
 	unicode: UNICODE.version_str,
@@ -341,8 +344,7 @@ writeFileSync(new URL('./nf.json', out_dir), JSON.stringify({
 }));
 writeFileSync(new URL('./nf-tests.json', out_dir), JSON.stringify(UNICODE.nf_tests()));
 
-// not important
-// convenience file for emoji.html
+// convenience file for emoji.html (not critical)
 writeFileSync(new URL('./emoji-info.json', out_dir), JSON.stringify([...emoji.values()].map(info => {
 	let {cps, name, version, type} = info;
 	return {form: String.fromCodePoint(...cps), name, version, type};
