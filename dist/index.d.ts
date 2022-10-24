a9f42a1634cc5955b424d16ad7a0a6bbfe626db1
Andrew Raffensperger <raffy@me.com>
2022-10-24 04:15:17 -0700

    update ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -37,14 +37,26 @@ interface StopToken {
 }
 type Token = TextToken | EmojiToken | NFCToken | StopToken;
 
+interface Label {
+	input: number[];
+	offset: number;
+	error?: string;
+	mapped?: [];
+	output?: number[];
+	emoji?: boolean;
+	script?: string;
+}
+
 export function ens_normalize(name: string): string;
 export function ens_beautify(name: string): string;
 
-export function ens_tokenize(name: string): Token[];
+export function ens_tokenize(name: string, options?: {nf?: boolean}): Token[];
+export function ens_split(name: string, emoji_filter?: (cps: number[]) => number[]): Label;
 export function ens_emoji(): number[][];
-
 export function ens_normalize_fragment(frag: string, nf?: (cps: number[]) => number[]): string;
-export function ens_normalize_post_check(norm: string): string;
 
 export function nfd(cps: number[]): number[];
 export function nfc(cps: number[]): number[];
+
+export function should_escape(cp: number): boolean;
+export function is_printable_mark(cp: number): boolean;
