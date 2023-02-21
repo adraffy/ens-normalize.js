115fa936abbbb61d7162976f76cc770c637c0156
Andrew Raffensperger <raffy@me.com>
2023-02-21 01:19:22 -0800

    resample tests after dingbat change

--- src/include-versions.js
+++ src/include-versions.js
@@ -2,5 +2,5 @@ export const derived = "2023-02-21T06:30:22.973Z";
 export const unicode = "15.0.0 (2022-10-23T06:00:57.990Z)";
 export const cldr = "42 (2022-11-04T04:55:37.180Z)";
 export const spec_hash = "962316964553fce6188e25a5166a4c1e906333adf53bdf2964c71dedc0f8e2c8";
-export const built = "2023-02-21T07:57:18.893Z";
+export const built = "2023-02-21T09:18:13.549Z";
 export const version = "1.9.0";
