0a92c492e83e6f2958bfdbecef501d21a6413bc5
Andrew Raffensperger <raffy@me.com>
2023-04-03 13:55:14 -0700

    update emoji regex for refund tool

--- tools/dump-no-refund.js
+++ tools/dump-no-refund.js
@@ -1,12 +1,37 @@
 import {writeFileSync} from 'node:fs';
 import {read_labels} from '../validate/data.js';
 import {ens_normalize, ens_tokenize} from '../src/lib.js';
-import {str_from_cps} from '../src/utils.js';
 import eth_ens_namehash from '../test/eth-ens-namehash@2.0.15.min.js';
 
-// from examples.js
+// see: https://github.com/adraffy/ens-normalize.js/blob/main/derive/rules/emoji.js#L116
+let non_rgi = [];
+function escape_cp(cp) {
+	return `\\u{${cp.toString(16)}}`;
+}
+// single regionals
+for (let cp = 0x1F1E6; cp <= 0x1F1FF; cp++) non_rgi.push(escape_cp(cp));
+// solo skin colors
+for (let cp = 0x1F3FB; cp <= 0x1F3FF; cp++) non_rgi.push(escape_cp(cp));
+// zwj sequences
+for (let emoji of [
+	// windows: https://blog.emojipedia.org/ninja-cat-the-windows-only-emoji/
+	'🐱‍🐉','🐱‍💻','🐱‍🚀','🐱‍👤','🐱‍🏍','🐱‍👓',
+	// man wrestler
+	'🤼🏻‍♂','🤼🏼‍♂','🤼🏽‍♂','🤼🏾‍♂','🤼🏿‍♂', 
+	// woman wrestler
+	'🤼🏻‍♀','🤼🏼‍♀','🤼🏽‍♀','🤼🏾‍♀','🤼🏿‍♀', 
+]) non_rgi.push(`${[...emoji].map(x => escape_cp(x.codePointAt(0)))}\uFE0F?`);
+const non_rgi_regex = new RegExp(`(${non_rgi.join('|')})`, 'ug');
+console.log(non_rgi_regex);
+
+/*
+/(\u{1f1e6}|\u{1f1e7}|\u{1f1e8}|\u{1f1e9}|\u{1f1ea}|\u{1f1eb}|\u{1f1ec}|\u{1f1ed}|\u{1f1ee}|\u{1f1ef}|\u{1f1f0}|\u{1f1f1}|\u{1f1f2}|\u{1f1f3}|\u{1f1f4}|\u{1f1f5}|\u{1f1f6}|\u{1f1f7}|\u{1f1f8}|\u{1f1f9}|\u{1f1fa}|\u{1f1fb}|\u{1f1fc}|\u{1f1fd}|\u{1f1fe}|\u{1f1ff}|\u{1f3fb}|\u{1f3fc}|\u{1f3fd}|\u{1f3fe}|\u{1f3ff}|\u{1f431},\u{200d},\u{1f409}️?|\u{1f431},\u{200d},\u{1f4bb}️?|\u{1f431},\u{200d},\u{1f680}️?|\u{1f431},\u{200d},\u{1f464}️?|\u{1f431},\u{200d},\u{1f3cd}️?|\u{1f431},\u{200d},\u{1f453}️?|\u{1f93c},\u{1f3fb},\u{200d},\u{2642}️?|\u{1f93c},\u{1f3fc},\u{200d},\u{2642}️?|\u{1f93c},\u{1f3fd},\u{200d},\u{2642}️?|\u{1f93c},\u{1f3fe},\u{200d},\u{2642}️?|\u{1f93c},\u{1f3ff},\u{200d},\u{2642}️?|\u{1f93c},\u{1f3fb},\u{200d},\u{2640}️?|\u{1f93c},\u{1f3fc},\u{200d},\u{2640}️?|\u{1f93c},\u{1f3fd},\u{200d},\u{2640}️?|\u{1f93c},\u{1f3fe},\u{200d},\u{2640}️?|\u{1f93c},\u{1f3ff},\u{200d},\u{2640}️?)/gu
+*/
+
+// remove real emoji and non-rgi emoji with FE0F allowance
+// keep everything else
 function filter_emoji(s) {
-	let cps = ens_tokenize(s).flatMap(token => {
+	return String.fromCodePoint(...ens_tokenize(s.replace(non_rgi_regex, '')).flatMap(token => {
 		switch (token.type) { 
 			case 'emoji': return []; // ignore
 			case 'nfc': return token.input; // pre-nfc
@@ -14,17 +39,11 @@ function filter_emoji(s) {
 			case 'valid': return token.cps;
 			default: return token.cp;
 		}
-	});
-	// account for single regionals
-	for (let pos = cps.length - 2; pos >= 0; pos--) {
-		let cp = cps[pos];
-		if (cp >= 0x1F1E6 && cp <= 0x1F1FF) {
-			let len = 1;
-			if (cps[pos+1] == 0xFE0F) len++;
-			cps.splice(pos, len);
-		}
-	}
-	return str_from_cps(cps);
+	}));
+}
+
+function is_invis_spoof(s) {
+	return /(\uFE0F|\uFE0E|\u200C|\u200D)/u.test(filter_emoji(s));
 }
 
 let found = 0;
@@ -44,13 +63,7 @@ for (let label of read_labels()) {
 	} catch (err) {
 	}
 	found++;
-	if (norm) {
-		// diff norm
-	} else {
-		// name now illegal
-	}
-	let emojiless = filter_emoji(label);
-	if (/(\uFE0F|\uFE0E|\u200C|\u200D)/u.test(emojiless)) {
+	if (is_invis_spoof(label)) {
 		spoof_invis.push(label);
 	}
 }
