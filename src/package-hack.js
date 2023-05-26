4b83520d0b2f66709bb943ac3ed7b2d42742d416
Andrew Raffensperger <raffy@me.com>
2023-05-25 20:36:44 -0700

    drop package module type during pack/publish

---
+++ src/package-hack.js
@@ -0,0 +1,23 @@
+import {readFileSync, writeFileSync} from 'node:fs';
+import {spawnSync} from 'node:child_process';
+
+let args = process.argv.slice(2);
+if (!args.length) {
+	throw new Error(`expected args`);
+}
+
+let file = new URL('../package.json', import.meta.url);
+let json = JSON.parse(readFileSync(file));
+
+function set_package_type(type) {
+	json.type = type;
+	console.log(`*** Package Type: ${type ?? '<unset>'} ***`);
+	writeFileSync(file, JSON.stringify(json, null, '\t'));
+}
+
+set_package_type();
+try {
+	spawnSync('npm', args, {stdio: 'inherit'});
+} finally {
+	set_package_type('module');
+}
