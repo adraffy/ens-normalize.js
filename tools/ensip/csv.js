073aa13b4bd86f19daeb1e815dfba4bf0f327ec4
Andrew Raffensperger <raffy@me.com>
2023-06-24 14:33:24 -0700

    add csv for ensip, fix vision url

--- tools/ensip/csv.js
+++ tools/ensip/csv.js
@@ -1,31 +1,36 @@
-import {writeFileSync} from 'node:fs';
-import {hex_cp, tt} from './utils.js';
-import {UNICODE, IDNA} from '../../derive/unicode-version.js';
+import {IDNA} from '../../derive/unicode-version.js';
 import DISALLOWED from '../../derive/rules/chars-disallow.js';
+import {read_spec} from '../../validate/data.js';
+import {write_csv} from './utils.js';
+
+const SPEC = read_spec();
 
 let allowed = new Set([
 	...IDNA.valid,
 	...IDNA.ignored,
 	...IDNA.mapped.map(v => v[0])
 ]);
-
 let disallowed = [...DISALLOWED].filter(cp => allowed.has(cp));
+
 // use default ordering, don't sort
 //disallowed.sort((a, b) => a - b);
 
-let lines = [];
-let rows = [];
-
-lines.push(`# Additional Disallowed Characters (${disallowed.length})`);
-rows.push('Codepoint,Form,Name');
-
 // TODO: figure out how to reveal some of these comments
+/*
+let lines = [];
+lines.push(`# Additional Disallowed Characters (${disallowed.length})`);
 lines.push(`| Codepoint | Form | Name |`);
 lines.push('| -: | :-: | :- |');
 for (let cp of disallowed) { 
 	lines.push(`| ${tt(hex_cp(cp))} | ${tt(String.fromCodePoint(cp))} | ${UNICODE.get_name(cp)} |`)
-	rows.push(`"${hex_cp(cp)}","${String.fromCodePoint(cp)}","${UNICODE.get_name(cp)}"`);
 }
-
 writeFileSync(new URL('./disallowed.md', import.meta.url), lines.join('\n'));
-writeFileSync(new URL('./disallowed.csv', import.meta.url), rows.join('\n'));
+*/
+
+write_csv('disallowed', disallowed);
+write_csv('ignored', SPEC.ignored,  {form: false});
+write_csv('escape', SPEC.escape, {form: false});
+write_csv('fenced', SPEC.fenced.map(x => x[0]));
+write_csv('nfc_check', SPEC.nfc_check);
+write_csv('nsm', SPEC.nsm);
+write_csv('cm', SPEC.cm);
--- tools/ensip/utils.js
+++ tools/ensip/utils.js
@@ -1,5 +1,7 @@
 import {hex_cp} from '../../src/utils.js';
+import {safe_str_from_cps} from '../../src/lib.js';
 import {UNICODE} from '../../derive/unicode-version.js';
+import {writeFileSync} from 'node:fs';
 
 export {hex_cp};
 
@@ -9,4 +11,28 @@ export function tt(s) {
 
 export function fmt_cp(cp) {
 	return tt(`${hex_cp(cp)} (${String.fromCodePoint(cp)}) ${UNICODE.get_name(cp, true)}`);
+}
+
+export function write_csv(name, recs, {form = true} = {}) {
+	let cols = ['Codepoint'];
+	if (form) cols.push('Form');
+	cols.push('Name');
+	let rows = [cols.join(',')];
+	for (let rec of recs) {
+		if (Number.isInteger(rec)) {
+			rec = {
+				cps: [rec],
+				name: UNICODE.get_name(rec)	
+			};
+		}
+		rows.push(cols.map(col => {
+			switch (col) {
+				case 'Codepoint': return rec.cps.map(hex_cp).join(' ');
+				case 'Name': return rec.name;
+				case 'Form': return safe_str_from_cps(rec.cps);
+				default: throw new Error(`unknown column: ${col}`);
+			}
+		}).map(x => typeof x === 'string' ? `"${x}"` : x).join(','));
+	}
+	writeFileSync(new URL(`./${name}.csv`, import.meta.url), rows.join('\n'));
 }
