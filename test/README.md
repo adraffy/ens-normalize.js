405c01d4c973c6a88a0d986752674ef9d60c966b
Andrew Raffensperger <raffy@me.com>
2023-08-09 23:23:56 -0700

    move refund stuff to ens-norm-tests, renames, add test readme, fix wrapped: error

---
+++ test/README.md
@@ -0,0 +1,17 @@
+* [`resolver.html`](./resolver.html) → [**Resolver Demo**](https://adraffy.github.io/ens-normalize.js/test/resolver.html)
+* [`chars.html`](./chars.html) → [Character Viewer](https://adraffy.github.io/ens-normalize.js/test/chars.html)
+* [`emoji.html`](./emoji.html) → [Supported Emoji](https://adraffy.github.io/ens-normalize.js/test/emoji.html)
+* [`confused.html`](./confused.html) → [Confused Explainer](https://adraffy.github.io/ens-normalize.js/test/confused.html)
+
+---
+
+* [`validate.html`](./validate.html) → [ENS Normalization Validation Tests](https://adraffy.github.io/ens-normalize.js/test/validate.html)
+* [`report-nf.html`](./report-nf.html) → [Unicode NormalizationTest](https://adraffy.github.io/ens-normalize.js/test/report-nf.html)
+* `npm run test` → [`coder.js`](./coder.js), [`nf.js`](./nf.js), [`validate.js`](./validate.js)
+
+---
+
+* [`eth-ens-namehash@2.0.15.min.js`](eth-ens-namehash@2.0.15.min.js) — [commit `f62f351`](https://github.com/ensdomains/eth-ens-namehash/commit/f62f351f8c646b337da2e1eb458a9ea091f35ce7) modified to build as ESM
+* [`examples.js`](./examples.js) — various code snippets
+* [`play.js`](./play.js) — [readme](../README.md) examples
+* [adraffy/ens-norm-tests](https://github.com/adraffy/ens-norm-tests) — additional normalization tests
---
+++ tools/ensip/create-csv-files.js
@@ -0,0 +1,40 @@
+import {IDNA} from '../../derive/unicode-version.js';
+import DISALLOWED from '../../derive/rules/chars-disallow.js';
+import {read_spec} from '../../validate/data.js';
+import {write_csv} from './utils.js';
+
+const SPEC = read_spec();
+
+let valid = new Set(SPEC.groups.flatMap(g => [...g.primary, ...g.secondary]));
+
+let idna_allowed = new Set([
+	...IDNA.valid,
+	...IDNA.ignored,
+	...IDNA.mapped.map(v => v[0])
+]);
+let additional_disallowed = [...DISALLOWED].filter(cp => idna_allowed.has(cp));
+
+
+// use default ordering, don't sort
+//disallowed.sort((a, b) => a - b);
+
+// TODO: figure out how to reveal some of these comments
+/*
+let lines = [];
+lines.push(`# Additional Disallowed Characters (${disallowed.length})`);
+lines.push(`| Codepoint | Form | Name |`);
+lines.push('| -: | :-: | :- |');
+for (let cp of disallowed) { 
+	lines.push(`| ${tt(hex_cp(cp))} | ${tt(String.fromCodePoint(cp))} | ${UNICODE.get_name(cp)} |`)
+}
+writeFileSync(new URL('./disallowed.md', import.meta.url), lines.join('\n'));
+*/
+
+write_csv('disallowed', additional_disallowed);
+write_csv('ignored', SPEC.ignored,  {form: false});
+write_csv('escape', SPEC.escape, {form: false});
+write_csv('fenced', SPEC.fenced.map(x => x[0]));
+write_csv('nfc_check', SPEC.nfc_check);
+write_csv('nsm', SPEC.nsm);
+write_csv('cm', SPEC.cm);
+write_csv('valid', [...valid].sort((a, b) => a - b));
