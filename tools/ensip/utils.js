bac19ed8cef49ca08a8bc6488f902d5a3aef0c23
Andrew Raffensperger <raffy@me.com>
2024-09-14 15:14:27 -0700

    fix quote issue

--- tools/ensip/utils.js
+++ tools/ensip/utils.js
@@ -32,7 +32,7 @@ export function write_csv(name, recs, {form = true} = {}) {
 				case 'Form': {
 					let form = String.fromCodePoint(...rec.cps);
 					if (is_combining_mark(rec.cps[0])) form = '◌' + form;
-					return form;
+					return form.replaceAll('"', '\\"');
 				}
 				default: throw new Error(`unknown column: ${col}`);
 			}
